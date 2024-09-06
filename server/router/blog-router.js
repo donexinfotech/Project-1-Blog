94d77568a64db83f2a82af815d93fd60163c6b6a
donexinfotech <donexinfotech@gmail.com>
2024-09-06 11:05:48 +0530

    made update backend

--- server/router/blog-router.js
+++ server/router/blog-router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {createBlog, getAllBlogs,deleteBlog} = require("../controllers/blog-controllers");
+const {createBlog, getAllBlogs,deleteBlog, updateBlog} = require("../controllers/blog-controllers");
 
 router.route("/create").post(createBlog);
 router.route("/get-blogs").get(getAllBlogs);
+router.route("/update-blog/:id").patch(updateBlog);
 
 module.exports = router;
