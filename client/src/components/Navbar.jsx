1ebaadedf03acd39fb674af4fba272921cd60210
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-09-07 19:47:33 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,33 +6,34 @@ const Navbar = () => {
   const { isLoggedIn, username, profilePicture, logout } = useAuth();
 
   return (
-    <nav className="bg-red-500 p-4">
+    <nav className="bg-gray-300 p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">Name</h1>
+        <h1 className="text-2xl font-bold">Name</h1>
         <div className="flex space-x-6 items-center">
-          <Link to="/" className="text-white hover:font-bold">Home</Link>
+          <div></div>
+          <Link to="/" className="hover:font-bold">HOME</Link>
 
           {/* Conditionally render Login/Logout and username/profile picture */}
           {isLoggedIn ? (
             <>
               <div className="flex items-center space-x-3">
-              <Link to="/create-blog" className="text-white hover:font-bold">Create</Link>
+              <Link to="/create-blog" className="hover:font-bold">CREATE</Link>
                 <button
                   onClick={logout}
-                  className="text-white hover:font-bold"
+                  className="hover:font-bold"
                 >
-                  Logout
+                  LOGOUT
                 </button>
                 <img
                   src={`data:image/jpeg;base64,${profilePicture}`}
                   alt="Profile"
                   className="w-8 h-8 rounded-full"
                 />
-                <span className="text-white font-medium">{username.toUpperCase()}</span>
+                <span className="font-medium">{username.toUpperCase()}</span>
               </div>
             </>
           ) : (
-            <Link to="/login" className="text-white hover:font-bold">Login</Link>
+            <Link to="/login" className="hover:font-bold">Login</Link>
           )}
 
         </div>
