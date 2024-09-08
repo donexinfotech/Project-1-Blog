1cc5787a64a0ced71054b7c027ef53a5c59db360
donexinfotech <donexinfotech@gmail.com>
2024-09-08 12:13:21 +0530

    added background gradient

--- client/src/components/pages/Pages.jsx
+++ client/src/components/pages/Pages.jsx
@@ -9,6 +9,10 @@ import UserProfile from '../user/userProfile';
 
 function Pages() {
   return (
+    <>
+    <div className="fixed top-0 -z-10 h-full w-full">
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
+    </div>
     <Router>
     <Navbar/>
       <Routes>
@@ -19,6 +23,7 @@ function Pages() {
         <Route path="/user/:id" element={<UserProfile/>}/>
       </Routes>
     </Router>
+    </>
   );
 }
 
