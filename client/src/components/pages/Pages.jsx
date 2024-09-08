2ce32da7485024983bd7a90b7d059b1d4e982ec1
donexinfotech <donexinfotech@gmail.com>
2024-09-08 12:19:27 +0530

    final gradent

--- client/src/components/pages/Pages.jsx
+++ client/src/components/pages/Pages.jsx
@@ -11,7 +11,7 @@ function Pages() {
   return (
     <>
     <div className="fixed top-0 -z-10 h-full w-full">
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
     </div>
     <Router>
     <Navbar/>
