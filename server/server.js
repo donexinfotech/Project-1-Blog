64c394cc50ba97bf3723a7fef29e249e37c0937f
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-10-10 23:31:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const commentRouter = require("./router/comment-router")
 const quizRouter = require("./router/quiz-router")
 
 app.use(cors({
-  origin: '*',
+  origin: ['https://cybiaware-donex.vercel.app','https://blogs-donex-backend.vercel.app'],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   allowedHeaders: 'Content-Type,Authorization'
 }))
