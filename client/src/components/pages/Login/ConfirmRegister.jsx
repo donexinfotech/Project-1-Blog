246bb81c864a72c23714ab6dba886f8dc399ac09
Shyam2615 <syb26633@gmail.com>
2024-11-07 23:40:23 +0530

    fix8

--- client/src/components/pages/Login/ConfirmRegister.jsx
+++ client/src/components/pages/Login/ConfirmRegister.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams  } from 'react-router-dom';
 
 const ConfirmRegister = () => {
     const { email } = useParams();
